d3e5f26e3dffe3249dc3790d7ac151f9385db6eb
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-09-15 18:26:24 -0700

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+const submitData = (name,email) =>{
+    return fetch("http://localhost:3000/users",
+    {
+        method : "POST",
+        headers : {
+            "Content-Type": "application/json",
+            "Accept" : "application/json"
+            
+        },
+        body: JSON.stringify({
+            "name":name,
+            "email":email
+        })
+    })
+    .then(resp => resp.json())
+    .then(body => {
+        document.querySelector("#response").textContent = body['id']
+    }).catch(err=>{document.querySelector("#response").textContent = err})
+}
